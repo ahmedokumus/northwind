5a46d75b3365a97ab6d09998d8eb380fa2e6568e
Ahmed Hakan OKUMUŞ <ahmed.aho666@outlook.com>
2023-06-15 22:20:19 +0300

    18. Gün Angular Giriş 3 Gün sonu commit

--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NEVER } from 'rxjs';
+import { Category } from 'src/app/models/category';
+import { CategoryService } from 'src/app/services/category.service';
 
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
-  styleUrls: ['./category.component.css']
+  styleUrls: ['./category.component.css'],
 })
-export class CategoryComponent {
+export class CategoryComponent implements OnInit {
+  categories: Category[];
+  currentCategory: Category | null;
 
+  constructor(private categoryService: CategoryService) {}
+
+  ngOnInit(): void {
+    this.getCategries();
+  }
+
+  getCategries() {
+    this.categoryService.getCategory().subscribe((response) => {
+      this.categories = response.data;
+    });
+  }
+
+  setCurrentCategory(category: Category) {
+    this.currentCategory = category;
+  }
+
+  getCurrentCategoryClass(category: Category) {
+    if (category == this.currentCategory) {
+      return 'list-group-item list-group-item-action active';
+    } else {
+      return 'list-group-item list-group-item-action';
+    }
+  }
+
+  getAllCategoryClass() {
+    if (!this.currentCategory) {
+      return 'list-group-item list-group-item-action active';
+    } else {
+      return 'list-group-item list-group-item-action';
+    }
+  }
+
+  deleteCurrentCategory() {
+    if (this.currentCategory) {
+      this.currentCategory = null;
+    }
+  }
 }
